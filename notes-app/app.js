cee73d154884f0a2cec07e913244cf45fe06192a
briecodes <hello@brie.work>
2020-07-09 21:51:41 -0400

    NPM modules exploration.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,7 @@
+// const getNotes = require('./notes.js');
+// const validator = require('validator');
+const chalk = require('chalk');
+
 // const add = require('./utils.js');
 
 // const sum = add(4, -2);
@@ -13,6 +17,21 @@
     4. From app.js, load in and call the function printing message to console
 */
 
-const getNotes = require('./notes.js');
+// const msg = getNotes();
+// console.log(msg);
 
-console.log(getNotes());
+
+// console.log(validator.isURL('brie.work'));
+
+/*
+    Challenge: Use the chalk libarary in your project
+
+    1. Intall version 2.4.1 of chalk
+    2. Load chalk into app.js
+    3. Use it to print the string "Success!" to the console in green
+    4. test your work
+
+    Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+*/
+
+console.log(chalk.green.bold.inverse('Success!'));
