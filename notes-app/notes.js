f7a1a2fe66173ea2c9f54871ab6b02d45e8199dc
briecodes <hello@brie.work>
2020-07-15 09:01:05 -0400

    Added duplicate conditional.

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -3,12 +3,20 @@ const getNotes = () => 'Your notes...';
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-  notes.push({
-    title: title,
-    body: body
-  });
+  const duplicateNotes = notes.filter( note => note.title === title );
 
-  saveNotes(notes);
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+  
+    saveNotes(notes);
+    console.log('New note added!');
+  } else {
+    console.log('Note title taken!');
+  };
+
 };
 
 const saveNotes = (notes) => {
